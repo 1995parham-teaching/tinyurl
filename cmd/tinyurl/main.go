b0fc3cf30cb27f9819a5b70589ac351e02795a26
Parham Alvani <parham.alvani@gmail.com>
2023-07-21 10:30:28 +0000

    feat: add main with banner

---
+++ cmd/tinyurl/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/pterm/pterm"
+	"github.com/pterm/pterm/putils"
+)
+
+func main() {
+	pterm.DefaultCenter.Println("Shorten your URL to easily remember them and share them with your clients")
+
+	s, _ := pterm.DefaultBigText.WithLetters(putils.LettersFromString("TinyURL")).Srender()
+	pterm.DefaultCenter.Println(s)
+
+	pterm.DefaultCenter.WithCenterEachLineSeparately().Println("Michael Weiss\nJuly 2023")
+}
