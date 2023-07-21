e91f43d0e2ac7f7f152f2c11b6ff63c356e22c03
Parham Alvani <parham.alvani@gmail.com>
2023-07-21 08:47:24 +0000

    feat: complete implementation for url sql repository.

--- internal/domain/model/url/repository.go
+++ internal/domain/model/url/repository.go
@@ -2,10 +2,13 @@ package url
 
 import (
 	"context"
+	"errors"
 )
 
+var ErrURLNotFound = errors.New("url does not exist")
+
 type Repository interface {
-	Create(context.Context, URL) (string, error)
+	Create(context.Context, URL) error
 	Update(context.Context, URL) error
 	FromShortURL(context.Context, string) (URL, error)
 }
--- internal/infra/repository/urldb.go
+++ internal/infra/repository/urldb.go
@@ -2,10 +2,12 @@ package repository
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"github.com/1989michael/tinyurl/internal/domain/model/url"
 	"github.com/1989michael/tinyurl/internal/infra/db"
+	"gorm.io/gorm"
 )
 
 type URLDB struct {
@@ -19,15 +21,31 @@ func NewURLDB(db *db.DB) *URLDB {
 }
 
 func (r *URLDB) Create(ctx context.Context, url url.URL) error {
-	if err := r.db.DB.WithContext(ctx).Create(&url).Error; err != nil {
+	if err := r.db.DB.WithContext(ctx).Save(&url).Error; err != nil {
 		return fmt.Errorf("url creation failed %w", err)
 	}
 
 	return nil
 }
 
-func (r *URLDB) GetWithShortURL() {
+func (r *URLDB) FromShortURL(ctx context.Context, key string) (url.URL, error) {
+	var instance url.URL
+
+	if err := r.db.DB.WithContext(ctx).Where("key = ?", key).First(&instance).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return instance, url.ErrURLNotFound
+		}
+
+		return instance, fmt.Errorf("fetching url from database failed %w", err)
+	}
+
+	return instance, nil
 }
 
-func (r *URLDB) Update() {
+func (r *URLDB) Update(ctx context.Context, url url.URL) error {
+	if err := r.db.DB.WithContext(ctx).Save(url).Error; err != nil {
+		return fmt.Errorf("updating url failed %w", err)
+	}
+
+	return nil
 }
