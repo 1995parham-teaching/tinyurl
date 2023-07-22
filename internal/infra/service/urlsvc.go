9235851baa28f1bfbe48b5b62b51c1ad7702a3c7
Parham Alvani <parham.alvani@gmail.com>
2023-07-22 19:41:06 +0000

    feat: add visit method url service

--- internal/infra/service/urlsvc.go
+++ internal/infra/service/urlsvc.go
@@ -10,11 +10,13 @@ import (
 
 	"github.com/1989michael/tinyurl/internal/domain/model/url"
 	"github.com/1989michael/tinyurl/internal/domain/repourl"
+	"go.uber.org/zap"
 )
 
 var (
 	ErrKeyGenFailed     = errors.New("cannot generate new random string as short url")
 	ErrKeyAlreadyExists = errors.New("given static key already exists")
+	ErrURLNotFound      = repourl.ErrURLNotFound
 )
 
 // shortURLKey generates a random key from the source characters.
@@ -38,7 +40,15 @@ func shortURLKey() string {
 }
 
 type URLSvc struct {
-	repo repourl.Repository
+	repo   repourl.Repository
+	logger *zap.Logger
+}
+
+func ProvideURLSvc(repo repourl.Repository, logger *zap.Logger) *URLSvc {
+	return &URLSvc{
+		repo:   repo,
+		logger: logger.Named("urlsvc"),
+	}
 }
 
 func (s *URLSvc) create(ctx context.Context, key string, address string, expire *time.Time) error {
@@ -56,18 +66,18 @@ func (s *URLSvc) create(ctx context.Context, key string, address string, expire 
 	return nil
 }
 
-func (s *URLSvc) Create(ctx context.Context, address string, expire *time.Time) error {
+func (s *URLSvc) Create(ctx context.Context, address string, expire *time.Time) (string, error) {
 	key := shortURLKey()
 
 	if err := s.create(ctx, key, address, expire); err != nil {
 		if errors.Is(err, repourl.ErrDuplicateShortURL) {
-			return ErrKeyGenFailed
+			return "", ErrKeyGenFailed
 		}
 
-		return fmt.Errorf("url creation failed %w", err)
+		return "", err
 	}
 
-	return nil
+	return key, nil
 }
 
 func (s *URLSvc) CreateWithKey(ctx context.Context, key string, address string, expire *time.Time) error {
@@ -78,8 +88,45 @@ func (s *URLSvc) CreateWithKey(ctx context.Context, key string, address string, 
 			return ErrKeyAlreadyExists
 		}
 
-		return fmt.Errorf("url creation failed %w", err)
+		return err
 	}
 
 	return nil
+}
+
+func (s *URLSvc) visit(ctx context.Context, key string) (url.URL, error) {
+	{
+		url, err := s.repo.FromShortURL(ctx, key)
+		if err != nil {
+			if !errors.Is(err, repourl.ErrURLNotFound) {
+				return url, fmt.Errorf("url fetching failed %w", err)
+			}
+		} else {
+			return url, nil
+		}
+	}
+
+	url, err := s.repo.FromShortURL(ctx, fmt.Sprintf("static_%s", key))
+	if err != nil {
+		if errors.Is(err, repourl.ErrURLNotFound) {
+			return url, ErrURLNotFound
+		}
+		return url, fmt.Errorf("url fetching failed %w", err)
+	}
+
+	return url, nil
+}
+
+func (s *URLSvc) Visit(ctx context.Context, key string) (url.URL, error) {
+	url, err := s.visit(ctx, key)
+	if err != nil {
+		return url, err
+	}
+
+	url.Visits += 1
+	if err := s.repo.Update(ctx, url); err != nil {
+		s.logger.Error("updating url visit coount failed", zap.Error(err))
+	}
+
+	return url, nil
 }
