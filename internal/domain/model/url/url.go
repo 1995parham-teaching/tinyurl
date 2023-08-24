5e349f995b9739a1b951dc66ffa298859af346a2
Parham Alvani <parham.alvani@gmail.com>
2023-08-24 22:13:46 +0000

    feat: add new constraints and have new migrations

--- internal/domain/model/url/url.go
+++ internal/domain/model/url/url.go
@@ -6,10 +6,10 @@ import (
 )
 
 type URL struct {
-	Key    string `gorm:"primaryKey"`
-	URL    string
-	Visits uint64
-	Expire sql.NullTime
+	Key    string       `gorm:"primaryKey"`
+	URL    string       `gorm:"index"`
+	Visits uint64       `gorm:"check:,visits >= 0"`
+	Expire sql.NullTime `gorm:"check:,expire > created_at"`
 
 	CreatedAt time.Time
 	UpdatedAt time.Time
